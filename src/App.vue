bace09f72a5783b62a9f65a70cea8ee29c92e33b
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2022-11-09 00:03:25 +0530

    feat: get all tabs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref, onMounted } from "vue";
+
+const sdf = ref(1);
+
+onMounted(() => {});
+
+const allUrls = computed(() => {
+  console.log("compute rerun");
+
+  const urls: string[] = [];
+
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach(function (tab) {
+      tab.url ? urls.push(tab.url) : null;
+    });
+  });
+
+  return urls;
+});
+</script>
 
 <template>
-  <div class="bg-blue-400">Hello</div>
+  <div v-if="allUrls" class="bg-blue-400">
+    <span v-for="url in allUrls">{{ url }}</span>
+  </div>
 </template>
 
 <style scoped></style>
