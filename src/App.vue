1131c1f4dd31bcd62562a68a0469a3c2d2eec167
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2022-12-07 19:26:59 +0530

    feat: add dummy data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { computed, ref, onMounted } from "vue";
-
-const sdf = ref(1);
+import { computed, onMounted } from "vue";
 
 onMounted(() => {});
 
 const allUrls = computed(() => {
-  console.log("compute rerun");
-
   const urls: string[] = [];
 
   chrome.tabs.query({}, (tabs) => {
@@ -16,13 +12,20 @@ const allUrls = computed(() => {
     });
   });
 
-  return urls;
+  // return urls;
+  return [
+    "chrome://newtab/",
+    "chrome://newtab/",
+    "https://www.google.co.in/",
+    "https://www.google.com/search?q=facebook&rlz=1C5CHFA_enIN983IN983&oq=facebook&aqs=chrome..69i57j0i131i433i512l2j0i512l3j0i131i433j0i131i433i512j0i512.1294j0j9&sourceid=chrome&ie=UTF-8",
+    "https://www.google.com/search?q=twitter&rlz=1C5CHFA_enIN983IN983&oq=twitter&aqs=chrome.0.0i271j46i67i131i199i433i465j35i39j0i67i131i433l2j0i131i433i512j0i433i512j0i131i433i512j0i131i433i457i512j0i402.1035j0j1&sourceid=chrome&ie=UTF-8",
+  ];
 });
 </script>
 
 <template>
   <div v-if="allUrls" class="bg-blue-400">
-    <span v-for="url in allUrls">{{ url }}</span>
+    <h2 v-for="url in allUrls">{{ url }}</h2>
   </div>
 </template>
 
