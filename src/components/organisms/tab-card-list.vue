0a0f0d430c43b93bb6be77987cc2cc01b8d225f5
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2022-12-07 20:43:58 +0530

    feat: move data to store

--- src/components/organisms/tab-card-list.vue
+++ src/components/organisms/tab-card-list.vue
@@ -1,34 +1,36 @@
 <script setup lang="ts">
+import { computed } from "vue";
+import { useTabsStore } from "../../store/tabs";
 import TabCard from "../molecules/tab-card.vue";
 
-const tabs = [
-  {
-    name: "Calvin Hawkins",
-    image:
-      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Kristen Ramos",
-    image:
-      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Ted Fox",
-    image:
-      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-];
+const tabsStore = useTabsStore();
+
+const allUrls = computed(() => {
+  const urls: string[] = [];
+
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach(function (tab) {
+      tab.url ? urls.push(tab.url) : null;
+    });
+  });
+  console.log("urls", urls);
+
+  return urls;
+});
 </script>
 
 <template>
-  <ul role="list" class="">
-    <TabCard
-      v-for="tab in tabs"
-      :key="tab.name"
-      :name="tab.name"
-      :image="tab.image"
-    />
-  </ul>
+  <div>
+    {{ allUrls }}
+    <ul role="list" class="">
+      <TabCard
+        v-for="tab in tabsStore.tabs"
+        :key="tab.name"
+        :name="tab.name"
+        :image="tab.image"
+      />
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
